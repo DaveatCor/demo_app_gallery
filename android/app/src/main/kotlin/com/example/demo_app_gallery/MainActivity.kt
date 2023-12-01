26fb9e2a1bbd933d4bb8a29b6f6390aba8341ff5
DaveatCor <condaveat@gmail.com>
2023-12-02 00:28:19 +0700

    complete the task

--- android/app/src/main/kotlin/com/example/demo_app_gallery/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.demo_app_gallery
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- lib/domain/contant/storage_key.const.dart
+++
@@ -1,6 +0,0 @@
-class StorageConstant {
-
-  static const listImage = "LIST_IMAGE";
-  static const connectivityResult = "CONNECTIVITY_RESULT";
-  
-}
